0c7983e4eddae8a089a3e5eb2f10f06199e6f16d
igor <borsbeck@student.21-school.ru>
2023-05-04 16:30:59 +0700

    config

---
+++ src/model/config.h
@@ -0,0 +1,62 @@
+#ifndef SRC_MODEL_CONFIG_H_
+#define SRC_MODEL_CONFIG_H_
+
+#include <vector>
+
+namespace s21 {
+
+enum class ModelType { kMatrix, kGraph };
+
+struct Metrics {
+  double accuracy = 0.0;
+  double precision = 0.0;
+  double recall = 0.0;
+  double f_measure = 0.0;
+  std::size_t time = 0u;
+};
+
+struct Architecture {
+  std::size_t hidden_layers = 2u;
+  std::size_t input_layer = 784u;
+  std::size_t hidden_layer = 140u;
+  std::size_t output_layer = 26u;
+};
+
+class Configuration {
+ public:
+  enum class TrainType { kTrain, kCrossValidation };
+
+  ModelType GetModelType() const { return model_type_; }
+  void SetModelType(ModelType type) { model_type_ = type; }
+
+  std::size_t GetHiddenLayers() const { return hidden_layers_; }
+  void SetHiddenLayers(std::size_t layers) { hidden_layers_ = layers; }
+
+  TrainType GetTrainType() const { return train_type_; }
+  void SetTrainType(TrainType type) { train_type_ = type; }
+
+  double GetTestSample() const { return test_sample_; }
+  void SetTestSample(double sample) { test_sample_ = sample; }
+
+  std::size_t GetKFolds() const { return k_folds_; }
+  void SetKFolds(std::size_t folds) { k_folds_ = folds; }
+
+  std::size_t GetEpochs() const { return epochs_; }
+  void SetEpochs(std::size_t epochs) { epochs_ = epochs; }
+
+  double GetLearningRate() const { return learning_rate_; }
+  void SetLearningRate(double rate) { learning_rate_ = rate; }
+
+ private:
+  ModelType model_type_ = ModelType::kMatrix;
+  std::size_t hidden_layers_ = 2u;
+  TrainType train_type_ = TrainType::kTrain;
+  double test_sample_ = 1.0;
+  std::size_t k_folds_ = 1u;
+  std::size_t epochs_ = 20u;
+  double learning_rate_ = 0.01;
+};
+
+}  // namespace s21
+
+#endif  // SRC_MODEL_CONFIG_H_
